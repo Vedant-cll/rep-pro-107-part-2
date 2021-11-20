c5c6f236ecfc5ca005927ff1a063f32d9da9c0e2
VV <85766798+Vedant-cll@users.noreply.github.com>
2021-11-20 10:51:53 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function StartClassification(){
+    navigator.mediaDevices.getUserMedia({audio: true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/qR9vg-UPV/model.json', modelReady);
+}
+function modelReady(){
+    classifier.classify(gotResults);
+}
+function gotResults(error, results){
+    if (error) {
+        console.error(error);
+      } else {
+        console.log(results);}
+}
